ba0a074c1de9eb2935c0b191b1a7be5660408c96
prana <prakashrana9847067614@gmail.com>
2020-03-23 18:33:56 -0400

    Initial Commit

---
+++ TabbedImage/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabbedImage
+//
+//  Created by Prakash on 2020-03-23.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabbedImage/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabbedImage
+//
+//  Created by Prakash on 2020-03-23.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
